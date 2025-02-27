3c52a84d1429c9a203fe5382f510dfd35c63ba3a
yashdeeptehlan <0yashdeeptehlan@gmail.com>
2025-02-26 19:30:49 -0500

    trying to connect to mongo db

--- backend/database/connection.py
+++ backend/database/connection.py
@@ -1,6 +1,10 @@
 from pymongo import MongoClient
 
-client = MongoClient("mongodb://localhost:27017/")
-db = client["crypto_data"]
+try:
+    client = MongoClient("mongodb://localhost:27017/ArbTradeX")
+    db = client["crypto_data"]
+    print("Connection established")
+except Exception as e:
+    print(f"Connection ERROR: {e}")
 
-
+real_time_collection = db["Exchange Prices"]
