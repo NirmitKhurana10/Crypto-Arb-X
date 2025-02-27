0dfaaeab8c9b05d72a86244ff8eb6b1107154a26
NirmitKhrurana10 <nkhurana1010@gmail.com>
2025-02-27 06:20:42 +0530

    added env variables to make it more robust

--- backend/database/connection.py
+++ backend/database/connection.py
@@ -1,6 +1,11 @@
+import os
+from dotenv import load_dotenv
 from pymongo import MongoClient
 
-client = MongoClient("mongodb://localhost:27017/ArbTradeX")
+load_dotenv()
+MONGO_URI = os.getenv("Mongo_URI")
+
+client = MongoClient(MONGO_URI)
 db = client["crypto_data"]
 
 
